ef6b5571677ffb0f8e0f72d0267df929befe3121
akazs <5749172+akazs@users.noreply.github.com>
2025-07-14 14:35:35 +0000

    cloudflare workers

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 export default {
-  kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    })
-  }
+	kit: {
+		adapter: adapter({
+			config: './wrangler.jsonc',
+			fallback: 'plaintext',
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
 };
